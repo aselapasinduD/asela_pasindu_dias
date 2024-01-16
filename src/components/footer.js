ad0a4f7e29cddaad2743bf3ae8942435e43a9eb7
Asela Pasindu <asela.pasindu.dias17@gmail.com>
2024-01-16 11:26:21 +0530

    fix PeoplePerHour not rendered problem

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,28 +1,34 @@
 //footer section
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import './styles/footer_quick_email.css'
 
 const Footer = () => {
-    const peoperPerHourHtml = `
-        <!--
-        PeoplePerHour Profile Widget
-        The div#pph-hire me is the element
-        where the iframe will be inserted.
-        You may move this element wherever
-        you need to display the widget
-        -->
-        <div id="pph-hireme"></div>
-        <script type="text/javascript">
-        (function(d, s) {
-            var useSSL = 'https:' == document.location.protocol;
-            var js, where = d.getElementsByTagName(s)[0],
-            js = d.createElement(s);
-            js.src = (useSSL ? 'https:' : 'http:') +  '//www.peopleperhour.com/hire/801905547/5255612.js?width=245&height=320&orientation=vertical&theme=dark&rnd='+parseInt(Math.random()*10000, 10);
-            try { where.parentNode.insertBefore(js, where); } catch (e) { if (typeof console !== 'undefined' && console.log && e.stack) { console.log(e.stack); } }
-        }(document, 'script'));
-        </script>
-    `;
+    const dataFatch = useRef(false);
+
+    useEffect(() => {
+        if(dataFatch.current) return;
+        const script = document.createElement('script');
+
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = 
+        (window.location.protocol === 'https:' ? 'https:' : 'http:') 
+        + '//www.peopleperhour.com/hire/801905547/5255612.js?width=245&height=320&orientation=vertical&theme=dark&rnd='
+        + parseInt(Math.random()*10000, 10);
+
+        script.onerror = function(e) {
+            if(typeof console !== 'undefined' && console.log && e.stack) {
+                console.log(e.stack);
+            }
+        };
+
+        document.body.appendChild(script);
+        dataFatch.current = true;
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, []);
 
     return (
         <footer id="footer" data-testid="footer" className=" sections Footer ">
@@ -31,7 +37,7 @@ const Footer = () => {
                 <ul className=" flex-1 ">
                     <li>asela.pasindu.dias17@gmail.com</li>
                     <li>Telephone - 0789991578 - 8am : 6pm</li>
-                    <li><div dangerouslySetInnerHTML={{__html: peoperPerHourHtml}}></div></li>
+                    <li id="pph-hireme"></li>
                 </ul>
                 <ul className=" flex-1 ">
                     <li>English - Professional</li>
@@ -76,8 +82,8 @@ const Footer = () => {
                 <p className=" text-center text-[15px] ">© 2024 Asela Pasindu Dias All rights reserved.<br/>Developed by VEBDRAKstudio.</p>
                 <p className=" absolute top-0 right-0 text-[12px]">Last Update - 2024-01 - 0.2v</p>
             </div>
-            
         </footer>
+        
     );
 }
 
